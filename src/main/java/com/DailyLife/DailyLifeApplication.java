22b406fa041f93ae9473eb081d8a37af00176006
DONGMIN SHIN <sdm03131229@gmail.com>
2022-02-15 17:56:32 +0900

    #2 test22

--- src/main/java/com/DailyLife/DailyLifeApplication.java
+++ src/main/java/com/DailyLife/DailyLifeApplication.java
@@ -9,6 +9,8 @@ public class DailyLifeApplication {
 	public static void main(String[] args) {
 
 		System.out.println("DailyLifeApplication.main");
+		System.out.println("DailyLifeApplication.main");
+		System.out.println("DailyLifeApplication.main");
 
 		SpringApplication.run(DailyLifeApplication.class, args);
 
