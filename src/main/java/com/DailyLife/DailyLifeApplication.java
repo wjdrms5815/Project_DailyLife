37f53701ea3e900b09c8b3828d6d75e412b77aed
DONGMIN SHIN <sdm03131229@gmail.com>
2022-02-15 17:53:56 +0900

    #2 test

--- src/main/java/com/DailyLife/DailyLifeApplication.java
+++ src/main/java/com/DailyLife/DailyLifeApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DailyLifeApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("DailyLifeApplication.main");
+
 		SpringApplication.run(DailyLifeApplication.class, args);
+
 	}
 
 }
